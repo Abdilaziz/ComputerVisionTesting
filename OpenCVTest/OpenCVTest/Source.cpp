f598f59beecc85b2d688645cbf3b7db99c1d1c8f
Abdi Ibrahim <azizib2nd@gmail.com>
2017-02-15 22:25:53 -0500

    Completed more about Template Filtering

--- OpenCVTest/OpenCVTest/Source.cpp
+++ OpenCVTest/OpenCVTest/Source.cpp
@@ -4,27 +4,39 @@
 #include <iostream>
 
 using namespace cv;
-using namespace std;
+//using namespace std;
 
 int main(int argc, char** argv)
 {
 	if (argc != 2)
 	{
-		cout << " Usage: display_image ImageToLoadAndDisplay" << endl;
+		std::cout << " Usage: display_image ImageToLoadAndDisplay" << std::endl;
 		return -1;
 	}
 
 	Mat image;
+
+
 	image = imread(argv[1], IMREAD_COLOR); // Read the file
 
 	if (image.empty()) // Check for invalid input
 	{
-		cout << "Could not open or find the image" << std::endl;
+		std::cout << "Could not open or find the image" << std::endl;
 		return -1;
 	}
-	Point i = 
-	line();
+	//Point i = Point(0, 400);
+	//Point y = Point(400, 400);
 
+	image = image.rowRange(Range(101, 103));
+	image = image.colRange(Range(201, 203));
+	for (int i = 0; i < image.rows; i++) {
+		for (int j = 0; j < image.cols; j++) {
+			std::cout  << image.at<double>(i, j) << std::endl;
+		}
+	}
+
+
+	printf("%d",image.rows);
 	namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
 	imshow("Display window", image); // Show our image inside it.
 
